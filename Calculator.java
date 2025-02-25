d282d04b9072d1776ece831f4614e9faf322a405
shreya-57 <shreyashakya222@gmail.com>
2025-02-25 09:04:42 +0545

    commit done by shreya

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,6 @@ public class Calculator{
     public static void main(String[] args) {
         System.out.println("inital commit");
         System.out.println("hello world");
-        System.out.println("for git meerge");
+        System.out.println("for git meerge modified");
     }
 }
