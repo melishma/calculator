47bc824fc7eb30c63e61e2dd4d86f8b0da2d9cef
shreya-57 <shreyashakya222@gmail.com>
2025-02-25 09:01:29 +0545

    first commit by shreya

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator{
+    public static void main(String[] args) {
+        System.out.println("inital commit");
+        System.out.println("hello world");
+        System.out.println("for git meerge");
+    }
+}
